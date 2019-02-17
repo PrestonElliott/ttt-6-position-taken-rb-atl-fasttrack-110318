a903786cec57a786a6dc51b0d6a5a1e8c3707304
Preston Elliott <preston.elliott24@gmail.com>
2019-02-17 17:52:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,16 +5,17 @@
 # index is a number to check 
 # index values are 0 through 8
 
-# SOLUTION
-# def position_taken?(board, index)
-#   !(board[index] == " " || board[index] == "" || board[index] == nil)
-# end
 
 def position_taken?(board, index)
-  position = board[index]
-  if position == "" || position == " " || position == nil
-    return false 
-  else
-    return true 
-  end
+  !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
+
+# ANOTHER SOLUTION
+# def position_taken?(board, index)
+#   position = board[index]
+#   if position == "" || position == " " || position == nil
+#     return false 
+#   else
+#     return true 
+#   end
+# end
