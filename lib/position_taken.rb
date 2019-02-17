7e352d37fdfeee4e1df2b1d9e9d24946675895fb
Preston Elliott <preston.elliott24@gmail.com>
2019-02-17 17:40:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 # board is an array of strings
 # ["X", "", " ", "0", etc]
 # index is a number to check 
-#index values are 0 through 8
+# index values are 0 through 8
 
 def position_taken?(board, index)
   !(board[index] == " " || board[index] == "" || board[index] == nil)
